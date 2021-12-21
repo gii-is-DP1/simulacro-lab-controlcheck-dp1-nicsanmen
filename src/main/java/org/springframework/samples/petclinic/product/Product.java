3b710449ea3bb50d1664a60da37d68a7834bc8e1
nicsanmen <nicsanmen@alum.us.es>
2021-12-21 18:37:38 +0100

    entrega 1

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,31 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
+@Entity
+public class Product extends NamedEntity{
+//    @Id
+//	Integer id;
+    
+    @NotNull
+    @Min(value = 3)
+    @Max(value = 50)
     String name;
+    
+    @NotNull
+    @PositiveOrZero
     double price;
+    
     ProductType productType;
 }
